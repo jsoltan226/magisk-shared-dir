7b6439e41a2321b27bf0d5ccdb8055dc9ccc1a6c
jsoltan226 <jsoltan226@github.com>
2024-05-20 23:54:02 +0200

    implement checking for CE userdata unlock status

--- service.sh
+++ service.sh
@@ -1,14 +1,18 @@
 #!/system/bin/sh
 > /cache/uwu.log
 i=0
-while [ -z "$(cmd -l | grep -w "vibrator")" ]; do
-    sleep 1
+wait_duration=1
+while [ "$(getprop sys.user.0.ce_available)" != 'true' ]; do
+    sleep "$wait_duration"
     i=$((i + 1))
-    if [ "$i" -gt 30 ]; then
-        echo vibrator not found after 30 seconds. Stop > /cache/uwu.log
-        exit 1
+    if [ "$i" -gt 100 ] && [ "$wait_duration" -eq 1 ]; then
+        echo "data not decrypted after 100 seconds." >> /cache/uwu.log
+        echo "entering long wait mode." >> /cache/uwu.log
+        wait_duration=10
     fi
 done
+
+echo "CE data unlocked!" >> /cache/uwu.log
 
 i=0
 while [ "$i" -lt 25 ]; do
